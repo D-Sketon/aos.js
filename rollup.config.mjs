bc9523d84ecb7de61fcfb40ab7f41671e65071f0
D-Sketon <2055272094@qq.com>
2024-10-05 10:09:10 +0800

    fix: add dependencies to bundle

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,23 +1,42 @@
-import typescript from '@rollup/plugin-typescript';
+import typescript from "@rollup/plugin-typescript";
+import terser from "@rollup/plugin-terser";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import scss from "rollup-plugin-scss";
 
 export default [
   {
-    input: './src/index.ts',
+    input: "./src/index.ts",
     output: [
       {
-        dir: 'dist',
-        format: 'cjs',
-        entryFileNames: 'aos.cjs.js',
+        dir: "dist",
+        format: "cjs",
+        entryFileNames: "aos.cjs.js",
         sourcemap: false,
       },
       {
-        dir: 'dist',
-        format: 'esm',
-        entryFileNames: 'aos.esm.js',
+        dir: "dist",
+        format: "esm",
+        entryFileNames: "aos.esm.js",
         sourcemap: false,
       },
+      {
+        dir: "./dist",
+        format: "umd",
+        entryFileNames: "aos.umd.js",
+        name: "AOS",
+        sourcemap: false,
+        plugins: [terser()],
+      },
     ],
-    plugins: [typescript({ module: "ESNext" })]
-  }
-]
-
+    plugins: [nodeResolve(), typescript({ module: "ESNext" })],
+  },
+  {
+    input: "./src/style/aos.scss",
+    plugins: [
+      scss({
+        outputStyle: "compressed",
+        output: "./dist/aos.css",
+      }),
+    ],
+  },
+];
