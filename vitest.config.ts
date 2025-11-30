a9fa6a68b3b2560ae2b9013f151e02853e6e186d
D-Sketon <2055272094@qq.com>
2025-11-30 20:03:21 +0800

    chore: update coverage reporter to include lcov format

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     environment: 'jsdom',
     coverage: {
       provider: 'v8',
-      reporter: ['text', 'json', 'html'],
+      reporter: ["lcov", "text", "html"],
       include: ['src/**/*.ts'],
       exclude: ['src/**/*.d.ts', 'src/style/**/*'],
     },
